8c6b5c3ef04d804a050dbb703ab83ce5156c9bb9
Kungfu-Chsia <allrighty@ya.ru>
2022-01-05 12:31:29 +0300

    автоматический выбор активного слайда

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
-const sliders = document.querySelectorAll('.slide')
-for (const slide of sliders) {
-  slide.addEventListener('click', () => {
-    clearActiveClasses()
-    slide.classList.add('active')
-  })
+function slidesPlugin(activeSlide = 0) {
+  const sliders = document.querySelectorAll('.slide')
+
+  sliders[activeSlide].classList.add('active')
+  for (const slide of sliders) {
+    slide.addEventListener('click', () => {
+      clearActiveClasses()
+      slide.classList.add('active')
+    })
+  }
+
+  function clearActiveClasses() {
+    sliders.forEach((slide) => {
+      slide.classList.remove('active')
+    })
+  }
 }
 
-function clearActiveClasses() {
-  sliders.forEach((slide) => {
-    slide.classList.remove('active')
-  })
-}
+slidesPlugin(2)
